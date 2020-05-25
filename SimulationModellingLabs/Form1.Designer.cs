dc913c4a99a334587d5a898a24fac866bbd69ae3
daniil.melnikov.1999 <daniil.melnikov.1999@mail.ru>
2020-05-25 17:30:26 +0700

    Init

--- SimulationModellingLabs/Form1.Designer.cs
+++ SimulationModellingLabs/Form1.Designer.cs
@@ -28,244 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Title title6 = new System.Windows.Forms.DataVisualization.Charting.Title();
-            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.prob1TextBox = new System.Windows.Forms.TextBox();
-            this.prob2TextBox = new System.Windows.Forms.TextBox();
-            this.prob3TextBox = new System.Windows.Forms.TextBox();
-            this.prob4TextBox = new System.Windows.Forms.TextBox();
-            this.prob5TextBox = new System.Windows.Forms.TextBox();
-            this.numOfExperimentsTextBox = new System.Windows.Forms.TextBox();
-            this.generateButton = new System.Windows.Forms.Button();
-            this.errorLabel = new System.Windows.Forms.Label();
+            this.display_richTextBox = new System.Windows.Forms.RichTextBox();
+            this.teams_richTextBox = new System.Windows.Forms.RichTextBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.averageLabel = new System.Windows.Forms.Label();
-            this.varianceLabel = new System.Windows.Forms.Label();
-            this.chiSquaredLabel = new System.Windows.Forms.Label();
-            this.chiSquaredLogicLabel = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
-            // chart1
+            // display_richTextBox
             // 
-            chartArea6.AxisX.IsStartedFromZero = false;
-            chartArea6.AxisX.MajorGrid.Enabled = false;
-            chartArea6.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea6);
-            this.chart1.Location = new System.Drawing.Point(337, 31);
-            this.chart1.Name = "chart1";
-            series6.ChartArea = "ChartArea1";
-            series6.IsValueShownAsLabel = true;
-            series6.Name = "Series1";
-            this.chart1.Series.Add(series6);
-            this.chart1.Size = new System.Drawing.Size(437, 291);
-            this.chart1.TabIndex = 0;
-            this.chart1.Text = "chart1";
-            title6.Alignment = System.Drawing.ContentAlignment.MiddleLeft;
-            title6.Name = "Title1";
-            title6.Text = "freq.";
-            this.chart1.Titles.Add(title6);
+            this.display_richTextBox.Location = new System.Drawing.Point(129, 192);
+            this.display_richTextBox.Name = "display_richTextBox";
+            this.display_richTextBox.Size = new System.Drawing.Size(542, 214);
+            this.display_richTextBox.TabIndex = 0;
+            this.display_richTextBox.Text = "";
             // 
-            // label1
+            // teams_richTextBox
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 68);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(69, 25);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Prob 1";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(12, 102);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(69, 25);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Prob 2";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(12, 136);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(69, 25);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "Prob 3";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(12, 171);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(69, 25);
-            this.label4.TabIndex = 4;
-            this.label4.Text = "Prob 4";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(12, 205);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(69, 25);
-            this.label5.TabIndex = 5;
-            this.label5.Text = "Prob 5";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(12, 258);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(213, 25);
-            this.label6.TabIndex = 6;
-            this.label6.Text = "Number of experiments";
-            // 
-            // prob1TextBox
-            // 
-            this.prob1TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.prob1TextBox.Location = new System.Drawing.Point(87, 66);
-            this.prob1TextBox.Name = "prob1TextBox";
-            this.prob1TextBox.Size = new System.Drawing.Size(81, 27);
-            this.prob1TextBox.TabIndex = 7;
-            this.prob1TextBox.Text = "0.264";
-            this.prob1TextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
-            // prob2TextBox
-            // 
-            this.prob2TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.prob2TextBox.Location = new System.Drawing.Point(87, 100);
-            this.prob2TextBox.Name = "prob2TextBox";
-            this.prob2TextBox.Size = new System.Drawing.Size(81, 27);
-            this.prob2TextBox.TabIndex = 8;
-            this.prob2TextBox.Text = "0.128";
-            this.prob2TextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
-            // prob3TextBox
-            // 
-            this.prob3TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.prob3TextBox.Location = new System.Drawing.Point(87, 134);
-            this.prob3TextBox.Name = "prob3TextBox";
-            this.prob3TextBox.Size = new System.Drawing.Size(81, 27);
-            this.prob3TextBox.TabIndex = 9;
-            this.prob3TextBox.Text = "0.228";
-            this.prob3TextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
-            // prob4TextBox
-            // 
-            this.prob4TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.prob4TextBox.Location = new System.Drawing.Point(87, 169);
-            this.prob4TextBox.Name = "prob4TextBox";
-            this.prob4TextBox.Size = new System.Drawing.Size(81, 27);
-            this.prob4TextBox.TabIndex = 10;
-            this.prob4TextBox.Text = "0.207";
-            this.prob4TextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
-            // prob5TextBox
-            // 
-            this.prob5TextBox.Enabled = false;
-            this.prob5TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.prob5TextBox.Location = new System.Drawing.Point(87, 203);
-            this.prob5TextBox.Name = "prob5TextBox";
-            this.prob5TextBox.Size = new System.Drawing.Size(81, 27);
-            this.prob5TextBox.TabIndex = 11;
-            this.prob5TextBox.Text = "auto";
-            this.prob5TextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
-            // numOfExperimentsTextBox
-            // 
-            this.numOfExperimentsTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.numOfExperimentsTextBox.Location = new System.Drawing.Point(242, 256);
-            this.numOfExperimentsTextBox.Name = "numOfExperimentsTextBox";
-            this.numOfExperimentsTextBox.Size = new System.Drawing.Size(75, 27);
-            this.numOfExperimentsTextBox.TabIndex = 12;
-            this.numOfExperimentsTextBox.Text = "100";
-            this.numOfExperimentsTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
-            // generateButton
-            // 
-            this.generateButton.BackColor = System.Drawing.Color.DodgerBlue;
-            this.generateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.generateButton.ForeColor = System.Drawing.Color.White;
-            this.generateButton.Location = new System.Drawing.Point(87, 321);
-            this.generateButton.Name = "generateButton";
-            this.generateButton.Size = new System.Drawing.Size(100, 42);
-            this.generateButton.TabIndex = 13;
-            this.generateButton.Text = "Start";
-            this.generateButton.UseVisualStyleBackColor = false;
-            this.generateButton.Click += new System.EventHandler(this.generateButton_Click);
-            // 
-            // errorLabel
-            // 
-            this.errorLabel.AutoSize = true;
-            this.errorLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.errorLabel.ForeColor = System.Drawing.Color.Red;
-            this.errorLabel.Location = new System.Drawing.Point(14, 380);
-            this.errorLabel.Name = "errorLabel";
-            this.errorLabel.Size = new System.Drawing.Size(0, 25);
-            this.errorLabel.TabIndex = 14;
+            this.teams_richTextBox.Location = new System.Drawing.Point(267, 27);
+            this.teams_richTextBox.Name = "teams_richTextBox";
+            this.teams_richTextBox.Size = new System.Drawing.Size(240, 85);
+            this.teams_richTextBox.TabIndex = 1;
+            this.teams_richTextBox.Text = "";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(205, 66);
+            this.button1.Location = new System.Drawing.Point(342, 138);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(59, 27);
-            this.button1.TabIndex = 15;
-            this.button1.Text = "Clear";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // averageLabel
-            // 
-            this.averageLabel.AutoSize = true;
-            this.averageLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.averageLabel.Location = new System.Drawing.Point(332, 338);
-            this.averageLabel.Name = "averageLabel";
-            this.averageLabel.Size = new System.Drawing.Size(92, 25);
-            this.averageLabel.TabIndex = 16;
-            this.averageLabel.Text = "Average:";
-            // 
-            // varianceLabel
-            // 
-            this.varianceLabel.AutoSize = true;
-            this.varianceLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.varianceLabel.Location = new System.Drawing.Point(332, 363);
-            this.varianceLabel.Name = "varianceLabel";
-            this.varianceLabel.Size = new System.Drawing.Size(96, 25);
-            this.varianceLabel.TabIndex = 17;
-            this.varianceLabel.Text = "Variance:";
-            // 
-            // chiSquaredLabel
-            // 
-            this.chiSquaredLabel.AutoSize = true;
-            this.chiSquaredLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.chiSquaredLabel.Location = new System.Drawing.Point(332, 402);
-            this.chiSquaredLabel.Name = "chiSquaredLabel";
-            this.chiSquaredLabel.Size = new System.Drawing.Size(126, 25);
-            this.chiSquaredLabel.TabIndex = 18;
-            this.chiSquaredLabel.Text = "Chi-squared:";
-            // 
-            // chiSquaredLogicLabel
-            // 
-            this.chiSquaredLogicLabel.AutoSize = true;
-            this.chiSquaredLogicLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.chiSquaredLogicLabel.Location = new System.Drawing.Point(647, 402);
-            this.chiSquaredLogicLabel.Name = "chiSquaredLogicLabel";
-            this.chiSquaredLogicLabel.Size = new System.Drawing.Size(0, 25);
-            this.chiSquaredLogicLabel.TabIndex = 19;
             // 
             // Form1
             // 
@@ -273,56 +64,20 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.chiSquaredLogicLabel);
-            this.Controls.Add(this.chiSquaredLabel);
-            this.Controls.Add(this.varianceLabel);
-            this.Controls.Add(this.averageLabel);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.errorLabel);
-            this.Controls.Add(this.generateButton);
-            this.Controls.Add(this.numOfExperimentsTextBox);
-            this.Controls.Add(this.prob5TextBox);
-            this.Controls.Add(this.prob4TextBox);
-            this.Controls.Add(this.prob3TextBox);
-            this.Controls.Add(this.prob2TextBox);
-            this.Controls.Add(this.prob1TextBox);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.teams_richTextBox);
+            this.Controls.Add(this.display_richTextBox);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox prob1TextBox;
-        private System.Windows.Forms.TextBox prob2TextBox;
-        private System.Windows.Forms.TextBox prob3TextBox;
-        private System.Windows.Forms.TextBox prob4TextBox;
-        private System.Windows.Forms.TextBox prob5TextBox;
-        private System.Windows.Forms.TextBox numOfExperimentsTextBox;
-        private System.Windows.Forms.Button generateButton;
-        private System.Windows.Forms.Label errorLabel;
+        private System.Windows.Forms.RichTextBox display_richTextBox;
+        private System.Windows.Forms.RichTextBox teams_richTextBox;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label averageLabel;
-        private System.Windows.Forms.Label varianceLabel;
-        private System.Windows.Forms.Label chiSquaredLabel;
-        private System.Windows.Forms.Label chiSquaredLogicLabel;
     }
 }
 
--- SimulationModellingLabs/Form1.cs
+++ SimulationModellingLabs/Form1.cs
@@ -13,168 +13,20 @@ namespace SimulationModellingLabs
 {
     public partial class Form1 : Form
     {
-        class Event
+        class Team
         {
-            public int x; // значения xi
-            public int n; // число выпадений значения xi
-            public double p; // вероятности значений xi
-            public double p_empirical; // pi называются относительными частотами(или эмпирическими вероятностями) значений xi
+            public string Name;
+            public int Win;
+            public int Lose;
+            public int Draw;
         }
 
         Random rnd = new Random();
-
-        int m = 5; 
-        int N = 0; // общее число экспериментов
-
-        double Ex; // матожидание
-        double Ex2; // матожидание квадрата
-        double Dx; // дисперсия
-        double Ex_empirical; // эмпирическое матожидание
-        double Ex2_empirical; // эмпирическое матожидание квадрата
-        double Dx_empirical; // эмпирическае дисперсия
-        double chiSquared; // критерий хи-квадрат Пирсона
-
-        List<Event> events;
 
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void generateButton_Click(object sender, EventArgs e)
-        {
-            events = new List<Event>();
-
-            for (int i = 1; i <= m; i++)
-            {
-                events.Add(new Event
-                {
-                    x = i
-                });
-            }
-
-            chart1.Series["Series1"].Points.Clear();
-            errorLabel.Text = "";
-            try
-            {
-                events.First(ev => ev.x == 1).p = GetDouble(prob1TextBox.Text, 0.16666);
-                events.First(ev => ev.x == 2).p = GetDouble(prob2TextBox.Text, 0.16666);
-                events.First(ev => ev.x == 3).p = GetDouble(prob3TextBox.Text, 0.16666);
-                events.First(ev => ev.x == 4).p = GetDouble(prob4TextBox.Text, 0.16666);
-                N = int.Parse(numOfExperimentsTextBox.Text);
-            }
-            catch
-            {
-                errorLabel.Text = "Error: Enter valid numbers";
-                return;
-            }
-
-            double sum = 0;
-            for (int i = 1; i <= 4; i++)
-            {
-                sum += events.First(ev => ev.x == i).p;
-            }
-
-            if (sum < 1)
-            {
-                events.First(ev => ev.x == 5).p = 1 - sum;
-            }
-            else
-            {
-                errorLabel.Text = "Error: Sum of probs > 1";
-                return;
-            }
-
-            for (int i = 0; i < N; i++)
-            {
-                var eventNum = generateEvent(rnd.NextDouble(), events.Select(ev => ev.p).ToArray());
-                events.First(ev => ev.x == eventNum).n++;
-            }
-
-            Ex = 0;
-            Ex2 = 0;
-            Dx = 0;
-            Ex_empirical = 0;
-            Ex2_empirical = 0;
-            Dx_empirical = 0;
-            chiSquared = 0;
-
-            foreach (Event ev in events)
-            {
-                ev.p_empirical = ev.n / (double)N;
-                chart1.Series["Series1"].Points.AddY(Math.Round(ev.p_empirical, 4));
-
-                Ex += ev.p * ev.x;
-                Ex2 += ev.p * Math.Pow(ev.x, 2);
-                Ex_empirical += ev.p_empirical * ev.x;
-                Ex2_empirical += ev.p_empirical * Math.Pow(ev.x, 2);
-                chiSquared += Math.Pow(ev.n, 2) / (N * ev.p); //Math.Pow(ev.n - N * ev.p, 2) / (N * ev.p);
-            }
-
-            chiSquared -= N;
-
-            Dx = Ex2 - Math.Pow(Ex, 2);
-            Dx_empirical = Ex2_empirical - Math.Pow(Ex_empirical, 2);
-
-            var deltaE = Math.Abs(Ex_empirical - Ex);
-            var deltaD = Math.Abs(Dx_empirical - Dx);
-            var deltaE_percents = deltaE / Math.Abs(Ex);
-            var deltaD_percents = deltaD / Math.Abs(Dx);
-
-            averageLabel.Text = $"Average: {Math.Round(Ex_empirical, 3)} (error = {(int)(deltaE_percents * 100)}%)";
-            varianceLabel.Text = $"Variance: {Math.Round(Dx_empirical, 3)} (error = {(int)(deltaD_percents * 100)}%)";
-            chiSquaredLabel.Text = $"Chi-squared: {Math.Round(chiSquared, 3)} > {11.07}    is ";
-            chiSquaredLogicLabel.Text = $"{Math.Round(chiSquared, 3) > 11.07}";
-            if (Math.Round(chiSquared, 3) > 11.07)
-            {
-                chiSquaredLogicLabel.ForeColor = Color.Green;
-            }
-            else
-            {
-                chiSquaredLogicLabel.ForeColor = Color.Red;
-            }
-        }
-
-        public static double GetDouble(string value, double defaultValue)
-        {
-            double result;
-
-            //Try parsing in the current culture
-            if (!double.TryParse(value, System.Globalization.NumberStyles.Any, CultureInfo.CurrentCulture, out result) &&
-                //Then try in US english
-                !double.TryParse(value, System.Globalization.NumberStyles.Any, CultureInfo.GetCultureInfo("en-US"), out result) &&
-                //Then in neutral language
-                !double.TryParse(value, System.Globalization.NumberStyles.Any, CultureInfo.InvariantCulture, out result))
-            {
-                result = defaultValue;
-                throw new Exception("Failed to parse from string");
-            }
-
-            return result;
-        }
-
-        private int generateEvent(double alpha, double[] eventsProb)
-        {
-            int k = 0;
-            double A = alpha;
-
-            A -= eventsProb[k];
-            while (A > 0 && k < m)
-            {
-                k++;
-                A -= eventsProb[k];
-            }
-
-            return k+1;
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            prob1TextBox.Text = "";
-            prob2TextBox.Text = "";
-            prob3TextBox.Text = "";
-            prob4TextBox.Text = "";
-            numOfExperimentsTextBox.Text = "";
-        }
     }
 }
