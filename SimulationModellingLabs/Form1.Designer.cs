0c1b504b3fe926570e9a9a6abd5040e694879a0a
daniil.melnikov.1999 <daniil.melnikov.1999@mail.ru>
2020-05-25 18:47:40 +0700

    Lab12 finished

--- SimulationModellingLabs/Form1.Designer.cs
+++ SimulationModellingLabs/Form1.Designer.cs
@@ -30,46 +30,66 @@
         {
             this.display_richTextBox = new System.Windows.Forms.RichTextBox();
             this.teams_richTextBox = new System.Windows.Forms.RichTextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.generateButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // display_richTextBox
             // 
-            this.display_richTextBox.Location = new System.Drawing.Point(129, 192);
+            this.display_richTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.display_richTextBox.Location = new System.Drawing.Point(161, 199);
             this.display_richTextBox.Name = "display_richTextBox";
-            this.display_richTextBox.Size = new System.Drawing.Size(542, 214);
+            this.display_richTextBox.Size = new System.Drawing.Size(459, 303);
             this.display_richTextBox.TabIndex = 0;
-            this.display_richTextBox.Text = "";
+            this.display_richTextBox.Text = "Team name - Points / Wins / Draws / Loses";
             // 
             // teams_richTextBox
             // 
-            this.teams_richTextBox.Location = new System.Drawing.Point(267, 27);
+            this.teams_richTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.teams_richTextBox.Location = new System.Drawing.Point(219, 41);
             this.teams_richTextBox.Name = "teams_richTextBox";
-            this.teams_richTextBox.Size = new System.Drawing.Size(240, 85);
+            this.teams_richTextBox.Size = new System.Drawing.Size(345, 85);
             this.teams_richTextBox.TabIndex = 1;
-            this.teams_richTextBox.Text = "";
+            this.teams_richTextBox.Text = "West Ham, Manchester City, Arsenal, Chealsea, Liverpool, Spurs, Everton, Manchest" +
+    "er United, Leister, Wolves";
             // 
-            // button1
+            // generateButton
             // 
-            this.button1.Location = new System.Drawing.Point(342, 138);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.generateButton.BackColor = System.Drawing.Color.DodgerBlue;
+            this.generateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.generateButton.ForeColor = System.Drawing.Color.White;
+            this.generateButton.Location = new System.Drawing.Point(333, 142);
+            this.generateButton.Name = "generateButton";
+            this.generateButton.Size = new System.Drawing.Size(100, 42);
+            this.generateButton.TabIndex = 14;
+            this.generateButton.Text = "Start";
+            this.generateButton.UseVisualStyleBackColor = false;
+            this.generateButton.Click += new System.EventHandler(this.generateButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(191, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(388, 29);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Enter team names with a comma \",\"";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(800, 514);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.generateButton);
             this.Controls.Add(this.teams_richTextBox);
             this.Controls.Add(this.display_richTextBox);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -77,7 +97,8 @@
 
         private System.Windows.Forms.RichTextBox display_richTextBox;
         private System.Windows.Forms.RichTextBox teams_richTextBox;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button generateButton;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- SimulationModellingLabs/Form1.cs
+++ SimulationModellingLabs/Form1.cs
@@ -19,14 +19,91 @@ namespace SimulationModellingLabs
             public int Win;
             public int Lose;
             public int Draw;
+            public int Points;
         }
 
         Random rnd = new Random();
+        double l = 2.7;
 
         public Form1()
         {
             InitializeComponent();
+            teams_richTextBox.Select();
         }
 
+        private void generateButton_Click(object sender, EventArgs e)
+        {
+            List<Team> teams = new List<Team>();
+
+            string[] teamNames = teams_richTextBox.Text.Split(',');
+            foreach (string teamName in teamNames)
+            {
+                if (teamName.Trim() == "") continue;
+                teams.Add(new Team
+                {
+                    Name = teamName.Trim(),
+                    Win = 0,
+                    Lose = 0,
+                    Draw = 0,
+                    Points = 0
+                });
+            }
+
+            for (int i = 0; i < teams.Count - 1; i++)
+            {
+                for (int j = i + 1; j < teams.Count; j++)
+                {
+                    double a1;
+                    double a2;
+                    int m1 = 0;
+                    int m2 = 0;
+                    double s1 = 0;
+                    double s2 = 0;
+
+                    while (s1 >= -l)
+                    {
+                        m1++;
+                        a1 = rnd.NextDouble();
+                        s1 += Math.Log(a1);
+                    }
+                    while (s2 >= -l)
+                    {
+                        m2++;
+                        a2 = rnd.NextDouble();
+                        s2 += Math.Log(a2);
+                    }
+
+                    if (m1 > m2)
+                    {
+                        teams[i].Win++;
+                        teams[i].Points += 3;
+                        teams[j].Lose++;
+                    }
+                    else if (m1 == m2)
+                    {
+                        teams[i].Draw++;
+                        teams[j].Draw++;
+                        teams[i].Points += 1;
+                        teams[j].Points += 1;
+                    }
+                    else
+                    {
+                        teams[j].Win++;
+                        teams[j].Points += 3;
+                        teams[i].Lose++;
+                    }
+                }
+            }
+
+            teams = teams.OrderByDescending(t => t.Points).ToList();
+
+            display_richTextBox.Clear();
+            display_richTextBox.Text += "Team name - Points / Wins / Draws / Loses\n\n";
+
+            foreach (Team team in teams)
+            {
+                display_richTextBox.Text += $"{team.Name} - {team.Points} / {team.Win} / {team.Draw} / {team.Lose}\n";
+            }
+        }
     }
 }
